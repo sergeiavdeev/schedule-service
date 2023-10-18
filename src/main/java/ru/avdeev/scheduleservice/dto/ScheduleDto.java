75648264ac5cfa06d465380dfaf7bfaf22a2fcd5
serg <avdey1978@gmail.com>
2023-10-18 23:14:59 +0300

    Deviation dto change

--- src/main/java/ru/avdeev/scheduleservice/dto/ScheduleDto.java
+++ src/main/java/ru/avdeev/scheduleservice/dto/ScheduleDto.java
@@ -2,13 +2,8 @@ package ru.avdeev.scheduleservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.Duration;
-import java.time.LocalTime;
-import java.time.Period;
 import java.util.List;
 import java.util.UUID;
 
