60495594e30081b19a32db6dbe3a5be6781caf55
serg <avdey1978@gmail.com>
2023-10-23 22:15:31 +0300

    Add indexes by calendarId and ownerId

--- src/main/resources/db/migration/V1.1__index.sql
+++ src/main/resources/db/migration/V1.1__index.sql
@@ -1,2 +1,8 @@
 create index calendar_owner_id_index
     on schedule.calendar (owner_id);
+
+create index deviation_calendar_id_index
+    on schedule.deviation (calendar_id);
+
+create index schedule_calendar_id_index
+    on schedule.schedule (calendar_id);
